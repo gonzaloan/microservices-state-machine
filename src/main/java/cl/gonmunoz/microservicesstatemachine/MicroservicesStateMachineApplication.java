6b15035cdad299b536ff2d4e31ddc64101e8436f
gonzalo.munoz <gonzaloan.munoz@gmail.com}>
2022-12-01 10:41:06 -0300

    Adds State Enums

---
+++ src/main/java/cl/gonmunoz/microservicesstatemachine/MicroservicesStateMachineApplication.java
@@ -0,0 +1,13 @@
+package cl.gonmunoz.microservicesstatemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesStateMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesStateMachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/gonmunoz/microservicesstatemachine/MicroservicesStateMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.gonmunoz.microservicesstatemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesStateMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
