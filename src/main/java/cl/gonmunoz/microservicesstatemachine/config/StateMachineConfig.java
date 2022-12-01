dc0f39cd2b681e2a751c2d42ccde19a563562824
gonzalo.munoz <gonzaloan.munoz@gmail.com}>
2022-12-01 11:05:47 -0300

    Adds test to check if state machine is working

--- src/main/java/cl/gonmunoz/microservicesstatemachine/config/StateMachineConfig.java
+++ src/main/java/cl/gonmunoz/microservicesstatemachine/config/StateMachineConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 
 import java.util.EnumSet;
 
@@ -25,5 +26,19 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
                 .end(PaymentState.AUTH) //End state successful
                 .end(PaymentState.PRE_AUTH_ERROR) //End state error
                 .end(PaymentState.AUTH_ERROR); //End state error
+    }
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<PaymentState, PaymentEvent> transitions) throws Exception {
+       transitions.withExternal().source(PaymentState.NEW)
+               .target(PaymentState.NEW) // We set events related with the states. So, new state, the event is to pre authorize
+               .event(PaymentEvent.PRE_AUTHORIZE)
+               .and()
+               // I go from New state to pre_auth when i get PRE_AUTH_APPROVED
+               .withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH).event(PaymentEvent.PRE_AUTH_APPROVED)
+               .and()
+               // I go from New state to pre_auth_ERROR when i get PRE_AUTH_DECLINED
+               .withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH_ERROR).event(PaymentEvent.PRE_AUTH_DECLINED);
+
     }
 }
